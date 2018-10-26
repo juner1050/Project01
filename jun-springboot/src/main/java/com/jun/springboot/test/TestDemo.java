edf4242e1edf0da731c507ddc3cf2a89bcb46d09
juner1050 <juner1050@126.com>
2018-10-26 11:28:00 +0800

    test - sts

--- jun-springboot/src/main/java/com/jun/springboot/test/TestDemo.java
+++ jun-springboot/src/main/java/com/jun/springboot/test/TestDemo.java
@@ -13,7 +13,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigBean.class);
 		UserService userService = context.getBean(UserService.class);
-		//测试冲突
+		//测试冲突  sts
 		List<User> list = userService.getUserList();
 		for (User user : list) {
 			System.out.println(user.getId() + "... " + user.getName() + "... " + user.getPassword());
